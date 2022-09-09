56ea2e6c7133ec1a33e3ed8d5b21ec366cf8eb5c
tusharmaSF <110518520+tusharmaSF@users.noreply.github.com>
2022-09-09 11:21:52 +0530

    Update user.model.ts

--- src/app/table/user.model.ts
+++ src/app/table/user.model.ts
@@ -1,4 +1,22 @@
 
+function getDate(){
+    return function(target: any, key: string){
+        let val = target[key];
+        const getter = () => {
+            return val;
+        };
+        const setter = (next: any) => {
+                let d = new Date();
+                val = d.toString();
+        };
+        Object.defineProperty(target, key,{
+            get: getter,
+            set: setter,
+            enumerable: true,
+            configurable: true,
+        })
+    };
+};
 
 enum Roles {
     Manager = "Manager",
@@ -10,6 +28,9 @@ enum Roles {
 
 export class UserModel{
 
+    @getDate()  
+    doe: string = '';
+
     id: number = 0;
     firstName: string = '';
     middleName: string = '';
@@ -18,5 +39,5 @@ export class UserModel{
     phoneNo: string = '';
     _Role: Roles = Roles.Subscriber;
     addRess: string = '';
-    doe: string = '';
+
 }
